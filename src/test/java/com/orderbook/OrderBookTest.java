12161c1b7f5e08e71cf7a36fdd02210d2926a938
Syahmim <syahmimcbs600@gmail.com>
2024-10-12 19:49:28 +0800

    Written OrderBookTest.java for unit testing

--- src/test/java/com/orderbook/OrderBookTest.java
+++ src/test/java/com/orderbook/OrderBookTest.java
@@ -1,66 +1,82 @@
 package com.orderbook;
 
+import com.orderbook.exchange.models.IOrder;
+import com.orderbook.exchange.models.Order;
+import com.orderbook.exchange.models.OrderType;
+import com.orderbook.exchange.orderbook.IOrderBook;
+import com.orderbook.exchange.orderbook.OrderBook;
+
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+
 
 public class OrderBookTest {
 
-    private OrderBook orderBook;
+    private IOrderBook orderBook;
 
     @Before
-    public void setUp() {
-        orderBook = new OrderBookImpl();
+    public void setup() {
+        orderBook = new OrderBook("AAPL");
     }
 
     @Test
-    public void testInitialStockQuantities() {
-        // Test initial stock quantities
-        assertEquals(100, orderBook.getStockQuantity("AAPL"));
-        assertEquals(200, orderBook.getStockQuantity("GOOGL"));
-        assertEquals(150, orderBook.getStockQuantity("TSLA"));
+    public void testAddBuyOrder() {
+        IOrder buyOrder = new Order("AAPL", OrderType.BUY, 150.00, 100);
+        orderBook.addOrder(buyOrder);
+
+        // Test if the order is added to the buyOrders queue
+        orderBook.printOrderBook();
     }
 
     @Test
-    public void testBuyOrder() throws InterruptedException {
-        // Buy 50 shares of AAPL
-        orderBook.placeOrder("AAPL", 50, true);
-        assertEquals(150, orderBook.getStockQuantity("AAPL"));
+    public void testAddSellOrder() {
+        IOrder sellOrder = new Order("AAPL", OrderType.SELL, 155.00, 50);
+        orderBook.addOrder(sellOrder);
+
+        // Test if the order is added to the sellOrders queue
+        orderBook.printOrderBook();
     }
 
     @Test
-    public void testSellOrder() throws InterruptedException {
-        // Sell 50 shares of GOOGL
-        orderBook.placeOrder("GOOGL", 50, false);
-        assertEquals(150, orderBook.getStockQuantity("GOOGL"));
+    public void testMatchOrders() {
+        IOrder buyOrder = new Order("AAPL", OrderType.BUY, 150.00, 100);
+        IOrder sellOrder = new Order("AAPL", OrderType.SELL, 145.00, 50);
+        
+        orderBook.addOrder(buyOrder);
+        orderBook.addOrder(sellOrder);
+
+        assertEquals(50, buyOrder.getQuantity());
+        assertEquals(0, sellOrder.getQuantity()); // sellOrder should be fully fulfilled and removed
+
     }
 
     @Test
-    public void testConcurrentOrders() throws InterruptedException {
-        // Simulate concurrent buy and sell orders using threads
-        Thread buyOrder = new Thread(() -> {
-            try {
-                orderBook.placeOrder("AAPL", 30, true);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+    public void testMultipleOrderMatching() {
+        // Add multiple buy and sell orders
+        IOrder buyOrder1 = new Order("AAPL", OrderType.BUY, 150.00, 100);
+        IOrder buyOrder2 = new Order("AAPL", OrderType.BUY, 155.00, 200);
+        IOrder sellOrder1 = new Order("AAPL", OrderType.SELL, 140.00, 50);
+        IOrder sellOrder2 = new Order("AAPL", OrderType.SELL, 145.00, 150);
 
-        Thread sellOrder = new Thread(() -> {
-            try {
-                orderBook.placeOrder("AAPL", 20, false);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        orderBook.addOrder(buyOrder1);
+        orderBook.addOrder(buyOrder2);
+        orderBook.addOrder(sellOrder1);
+        orderBook.addOrder(sellOrder2);
 
-        buyOrder.start();
-        sellOrder.start();
+        // Expectation: buyOrder2 should be prioritized because its price is higher
+        // sellOrder1 should match with buyOrder2
+        // sellOrder2 should match partially with buyOrder2 and buyOrder1
 
-        buyOrder.join();
-        sellOrder.join();
+        orderBook.printOrderBook();
 
-        // Final quantity of AAPL should reflect both buy and sell operations
-        assertEquals(110, orderBook.getStockQuantity("AAPL"));
+        // Further assertions (e.g., using reflection or extending IOrderBook to expose internal state)
+        assertEquals(100, buyOrder1.getQuantity());
+        assertEquals(0, sellOrder1.getQuantity());
+        assertEquals(0, buyOrder2.getQuantity());
+        assertEquals(0, sellOrder2.getQuantity());
+
+
     }
 }
